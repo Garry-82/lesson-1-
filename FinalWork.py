9a03f7e1c4c7d74dccefe74ca7643194b650daa9
Garry-82 <168720206+Garry-82@users.noreply.github.com>
2024-05-12 15:28:33 +0300

    Add files via upload

---
+++ FinalWork.py
@@ -0,0 +1,32 @@
+grades=[[5,4,4,3,5,5,5],[5,5,5,4,5,5],[3,5,5,4,5,4,5],[4,4,4,5,4,5],[3,3,3,2,3,3,4],[5,5,5,5,5,5,5]]
+students={'Roman',"Maks","Segr","Andrey","Igor","Vadim"}
+print("Условие задания. Дано:")
+print("- оценки учеников: ",grades)
+print("- имена учеников:  ",students)
+
+if len(grades)==len(students):
+    students=list(students)
+    students.sort()
+    #sorted_students=sorted(students) # - еще один метод сортировки
+    print("отсортированный список учеников: ",students)
+    a=0   # - переменная, счетчик учеников
+    dict_students= {}  # - переменная итогового словаря
+    while a<len(students):
+        average_score=sum(grades[a])/len(grades[a])
+        dict_students[students[a]] = average_score
+        a=a+1
+else:
+    print("количество учеников и их оценок не совпадает. БУДЬТЕ ВНИМАТЕЛЬНЕЕ!")
+
+print("вывод словаря учеников с указанием их среднего балла: ")
+print(dict_students)
+
+#how_mach_students=len(students)
+# print(how_mach_students)
+# a=0 #указатель на индекс ученика
+#
+# while a+1<how_mach_students:
+#     if students[a+1]<students[a]:
+#     str1=students[]
+# else:
+#     print('finish')
